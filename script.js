3b93bd356385c0869fca89b2e3f2c798f0e25520
VijaySelvaraj19 <selvarajvijay03@gmail.com>
2024-07-30 10:57:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
+const signInBtnLink = document.querySelector('.signInBtn-link');
+const wrapper = document.querySelector('.wrapper');
+
+signUpBtnLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    wrapper.classList.add('active');
+});
+
+signInBtnLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    wrapper.classList.remove('active');
+});
